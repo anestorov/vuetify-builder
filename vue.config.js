6678de3e0913e13fd789e5eaf9b06bc92332deb5
Asparuh Nestorov <anestorov@gmail.com>
2022-09-21 14:41:54 +0300

    live

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
+  outputDir: "./docs",
 
   //runtimeCompiler: true
 
